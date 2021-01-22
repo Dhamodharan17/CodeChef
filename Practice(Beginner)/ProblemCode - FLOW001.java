8405dda9c5837cff455ff13de5de251c697f0d85
Karthik <30789057+Dhamodharan17@users.noreply.github.com>
2021-01-22 09:56:47 +0530

    Add files via upload

---
+++ Practice(Beginner)/ProblemCode - FLOW001.java
@@ -0,0 +1,15 @@
+import java.util.*;
+// Remember that the class name should be "Main" and should be "public".
+public class Main {
+	public static void main(String[] args) {
+	     Scanner input= new Scanner(System.in);
+	     int t=input.nextInt();
+	    for(int i=0;i<t;i++)
+	    {
+	        int a=input.nextInt();;
+	        int b=input.nextInt();
+	        System.out.println(a+b);
+	    }
+
+	}
+}
